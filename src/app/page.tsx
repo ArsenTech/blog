b8da97485ff0e7c15472e48063279dbb626dc245
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2025-09-11 23:35:57 +0400

    Fixed `The path argument must be of type string or an instance of Buffer or URL. Received an instance of URL` error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import PageLayout from "@/components/layout";
 import LandingPage from "@/components/pages/landing";
 import { POSTS_PER_PAGE } from "@/lib/constants";
 import { getAllPosts, getCategories } from "@/lib/helpers";
-import generateRSS from "@/lib/rss";
 
 interface HomepageProps{
   searchParams: Promise<{query?: string, page?: string, pageSize?:string}>
@@ -14,7 +13,6 @@ export default async function Home({searchParams}: HomepageProps) {
   const posts = await getAllPosts();
   const categories = await getCategories(posts);
   const totalPages = Math.ceil(posts.length / postsPerPage)
-  await generateRSS(posts)
   return (
     <PageLayout>
       <LandingPage
